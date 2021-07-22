f6dce6eeac414bec249fcb233d8676f69a5e9bba
bernasjava <61662426+bernasjava@users.noreply.github.com>
2021-07-22 22:55:04 +0200

    confirmation token service

--- src/main/java/com/spring/register/registration/token/ConfirmationTokenService.java
+++ src/main/java/com/spring/register/registration/token/ConfirmationTokenService.java
@@ -2,6 +2,10 @@ package com.spring.register.registration.token;
 
 public class ConfirmationTokenService {
 
+    private final ConfirmationTokenRepository confirmationTokenRepository;
 
+    public void saveConfirmationToken(ConfirmationToken confirmationToken) {
+        confirmationTokenRepository.save(confirmationToken);
+    }
 
 }
